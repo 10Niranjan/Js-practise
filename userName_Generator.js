7dcb58920149c92e6d82dc386958ab4858be539f
Niranjan <121683247+10Niranjan@users.noreply.github.com>
2025-10-02 11:49:28 +0530

    Add files via upload

---
+++ userName_Generator.js
@@ -0,0 +1,7 @@
+let fullName = prompt("Enter your fullname without spaces: ");
+
+let username = "@" + fullName + fullName.length;
+
+console.log(fullName);
+console.log(username);
+alert("Your username is: " + username); 
